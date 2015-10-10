c97856734679ff3338f0b1d6b13e98c154bf37f7
Valter Strods <valterstrods@aospa.co>
2015-10-11 02:54:10 +0300

    Nuke Paranoid OTA
    We are starting off fresh. That's all.
    
    Change-Id: I64cd763c8043e879a963744bb20b6d0043beb8ce

--- Android.mk
+++ Android.mk
@@ -1,24 +1,29 @@
-LOCAL_PATH:= $(call my-dir)
+# ParanoidOTA: Provider of Paranoid Android Over the Air (OTA) updates.
+# Copyright (C) 2015 ParanoidAndroid Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4_13 \
-    volley
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PACKAGE_NAME := ParanoidOTA
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SDK_VERSION := 21
-
 include $(BUILD_PACKAGE)
-
-# Support library v4
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    android-support-v4_13:/libs/android-support-v4.jar
-
-include $(BUILD_MULTI_PREBUILT)
